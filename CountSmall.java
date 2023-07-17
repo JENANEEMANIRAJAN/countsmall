0bc0800d769c0d12561cf05bb94e7a70d8d04325
JENANEEMANIRAJAN <jenanee1129@gmail.com>
2023-07-17 12:59:46 +0530

    count

--- CountSmall.java
+++ CountSmall.java
@@ -1,9 +1,36 @@
 package countSmall;
 
-public class CountSmall {
+import java.util.Arrays;
+import java.util.Scanner;
+
+class CountSmall {
+	 static void Arr(int arr[], int Small[], int a) {
+		 for(int i = 0; i<a; i++) {
+			 Small[i] = 0;
+			 for(int j = i+1; j<a; j++) {
+				 if(arr[j]<arr[i])
+					 Small[i]++;
+					 
+			 }
+		 }
+	 }
+	 static void Arr(int arr[], int size) {
+		 System.out.println(Arrays.toString(arr));
+	 }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int arr[] = new int[a];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int b = arr.length;
+		int c[] = new int[b];
+		CountSmall.Arr(arr, c, b);
+		CountSmall.Arr(c, b);
+		
+		
 
 	}
 
